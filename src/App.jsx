9f200c461df4ac3df236f4b9960e8805836ea3c6
Jesperr <Info.brokeneu@gmail.com>
2022-12-14 16:14:38 +0100

    First push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+    return (
+        <div id='container' className='w-full h-screen flex justify-center items-center'>
+            <div className='w-2/3 h-2/3 border-2'></div>
+        </div>
+    )
+}
+
+export default App
